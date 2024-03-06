d7b539618b083a884e5704ab831e2bfb670da9d8
wasd <wasd@wasd.xyz>
2024-03-06 19:41:14 +0100

    added mediatr package

--- HotelBackendV2/obj/Debug/net8.0/HotelBackendV2.AssemblyInfo.cs
+++ HotelBackendV2/obj/Debug/net8.0/HotelBackendV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBackendV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9803ff8dbf7c23bb673e5dc9a76766828a9e21a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd89aa59f53c56313e8bb57f9b2dcc08229c84b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBackendV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBackendV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
